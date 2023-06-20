702bc1e00df2389da741e3b89f2d0c60b6d254c0
Sugam Phirke <sugamphirke9@gmail.com>
2023-06-20 13:08:30 +0530

    Added Coupon Functionality

--- FrontEnd/src/app/cart/cart.component.ts
+++ FrontEnd/src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { cart, priceSummary } from 'src/data-type';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cart',
@@ -9,7 +10,14 @@ import { cart, priceSummary } from 'src/data-type';
 })
 
 export class CartComponent implements OnInit {
-  constructor(private product: ProductService) { }
+
+  constructor(private product: ProductService, private http: HttpClient) {
+    this.validCoupon = false;
+    this.invalidCoupon = false;
+    this.showMessage = false;
+    this.showRemoveButton = false;
+  }
+
   myData: string = '';
   cartData: cart[] | undefined;
   lockIconSrc: string = "assets/Photos/back_idle.png";
@@ -39,7 +47,7 @@ export class CartComponent implements OnInit {
           console.log(item.quantity);
         } else {
           console.log('Quantity is missing:', item.quantity);
-          price += +item.price; 
+          price += +item.price;
         }
       });
       this.priceSummary.price = price;
@@ -77,7 +85,7 @@ export class CartComponent implements OnInit {
     this.product.currentCart().subscribe((result) => {
       const productId = result[i].productId;
       this.product.updateQuantity({ "quantity": this.selectedQuantity }, productId);
-      setTimeout(()=>{
+      setTimeout(() => {
         window.location.reload();
       }, 1000);
     });
@@ -110,4 +118,105 @@ export class CartComponent implements OnInit {
       }, 3000);
     });
   }
+
+  couponCode: string = '';
+  validCoupon: boolean;
+  invalidCoupon: boolean;
+  showMessage: boolean;
+  showRemoveButton: boolean;
+
+  applyCoupon() {
+    this.http.get<any[]>('http://localhost:3000/coupons')
+      .subscribe((coupons: any[]) => {
+        const coupon = coupons.find(c => c.code === this.couponCode);
+        if (coupon && coupon.code==="TESTING") {
+          this.validCoupon = true;
+          this.invalidCoupon = false;
+
+          this.product.currentCart().subscribe((result) => {
+            this.cartData = result;
+            let price = 0;
+            result.forEach((item) => {
+              if (item.quantity) {
+                price += +item.price * item.quantity;
+                console.log(item.quantity);
+              } else {
+                console.log('Quantity is missing:', item.quantity);
+                price += +item.price;
+              }
+            });
+            this.priceSummary.price = price;
+            const taxRate = 0.13;
+            this.priceSummary.tax = parseFloat((price * taxRate).toFixed(2));
+            this.priceSummary.delivery = 125;            
+            let totalMoney = price + 125;
+            this.priceSummary.discount = parseFloat(((price * .08) + (totalMoney - 1)).toFixed(2));
+            totalMoney = 1;
+            this.priceSummary.total = totalMoney;
+          });
+          this.showRemoveButton = true;
+        } 
+        else if(coupon){
+          this.product.currentCart().subscribe((result) => {
+            this.cartData = result;
+            let price = 0;
+            result.forEach((item) => {
+              if (item.quantity) {
+                price += +item.price * item.quantity;
+                console.log(item.quantity);
+              } else {
+                console.log('Quantity is missing:', item.quantity);
+                price += +item.price;
+              }
+            });
+            this.priceSummary.price = price;
+            const taxRate = 0.13;
+            this.priceSummary.tax = parseFloat((price * taxRate).toFixed(2));
+            this.priceSummary.delivery = 125;            
+            let totalMoney = price + 125;
+            this.priceSummary.discount = parseFloat(((price * .08) + (totalMoney * coupon.percent * 0.01)).toFixed(2));
+            totalMoney = parseFloat((totalMoney - (totalMoney * coupon.percent / 100)).toFixed(2));
+            this.priceSummary.total = totalMoney;
+          });
+          this.validCoupon = true;
+          this.invalidCoupon = false;
+          this.showRemoveButton = true;
+        }
+        else {
+          this.validCoupon = false;
+          this.invalidCoupon = true;
+        }
+        this.showMessage = true;
+      });
+  }
+
+  removeCoupon() {
+    this.product.currentCart().subscribe((result) => {
+      this.cartData = result;
+      let price = 0;
+      result.forEach((item) => {
+        if (item.quantity) {
+          price += +item.price * item.quantity;
+          console.log(item.quantity);
+        } else {
+          console.log('Quantity is missing:', item.quantity);
+          price += +item.price;
+        }
+      });
+      this.priceSummary.price = price;
+      this.priceSummary.discount = price * .08;
+      const taxRate = 0.13;
+      this.priceSummary.tax = parseFloat((price * taxRate).toFixed(2));
+      this.priceSummary.delivery = 125;
+      let totalMoney = price + 125;
+      this.priceSummary.total = totalMoney;
+    });
+    this.couponCode = '';
+    this.validCoupon = false;
+    this.invalidCoupon = false;
+    this.showMessage = false;
+    this.showRemoveButton = false;
+  }
+
+
 }
